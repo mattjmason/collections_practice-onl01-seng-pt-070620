f70f2f84a5bd6274a5a1faf79fbdb385dc13ba28
Matt Mason <masonmj10@gmail.com>
2020-07-09 19:32:37 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,51 @@
 
+def sort_array_asc(array)
+ array.sort do |a, b| 
+   a <=> b 
+   end 
+end 
+
+def sort_array_desc(array)
+ array.sort.reverse do |a, b| 
+   a <=> b 
+   end 
+end 
+
+def sort_array_char_count(array)
+ array.sort do |a, b| a.length - b.length 
+   end 
+end 
+
+
+def swap_elements(array)
+ array.sort do |a, b| a.length - b.length 
+   end 
+end 
+
+def reverse_array(array)
+ array [1,2] = array[2,1]
+end 
+
+def reverse_array(array)
+ array.reverse 
+end 
+
+
+def kesha_maker(array)
+ array.map { |name| name[2] = "$"} 
+ array 
+end 
+
+def find_a(array)
+  array.select { |item| item[0] == "a"}
+end 
+
+def sum_array(array)
+  array.inject(:+)
+end 
+
+def add_s(array)
+  array.map.with_index do |item, index|
+    index != 1 ? item << "s" : item 
+  end 
+end 
